f0b5e5cd99ab394005f71b3edf82b34d925fb341
SamuelLJG <samuelljg@outlook.com>
2026-01-17 04:05:43 -0300

    test

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,27 +1,72 @@
- 
- type Props = {
+type Props = {
   params: Promise<{ id: string }>
 }
 
-export async function generateStaticParams() {
-  return [
-    { id: '10000045' }
-  ]
+type CharacterItems = Record<
+  string,
+  {
+    id: number
+    name: string
+    route: string
+  }
+>
+function normalize(str: string) {
+  return str
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/\s+/g, '-')
 }
-
-export default async function Page({
-  params,
-}: Props) {
-   const id = (await params).id;
+export async function generateStaticParams() {
   const res = await fetch(
-    `https://gi.yatta.moe/api/v2/pt/avatar/${id}?vh=63F3`,
+    'https://gi.yatta.moe/api/v2/pt/avatar',
     { cache: 'force-cache' }
   )
 
-  const character = await res.json()
+  const json = await res.json()
+
+  const items: CharacterItems = json.data.items
+
+  return Object.values(items).map((char) => ({
+    slug: normalize(char.route || char.name)
+  }))
+  
+}
+
+export default async function Page({ params }: Props) {
+  const id = (await params).id;
+  const res = await fetch(
+    'https://gi.yatta.moe/api/v2/pt/avatar',
+    { cache: 'force-cache' }
+  )
+
+  const json = await res.json()
+  const items: CharacterItems = json.data.items
+
+  const slug = normalize(id)
+
+  const character = Object.values(items).find((char) => {
+    return (
+      normalize(char.route) === slug ||
+      normalize(char.name) === slug
+    )
+  })
+
+  if (!character) {
+    throw new Error('Personagem não encontrado')
+  }
+
+  const characterRes = await fetch(
+    `https://gi.yatta.moe/api/v2/pt/avatar/${character.id}?vh=63F3`,
+    { cache: 'force-cache' }
+  )
+
+  const characterData = await characterRes.json()
+
   return (
-    <ul>
-      <li>{character.data.fetter.title}</li>
-    </ul>
+    <div>
+      <h1>{characterData.data.name}</h1>
+      <p>{characterData.data.fetter.title}</p>
+    </div>
   )
 }
