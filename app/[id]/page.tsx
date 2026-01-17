1fdc3e927da775482c251230057e679969149ec2
SamuelLJG <samuelljg@outlook.com>
2026-01-17 03:28:50 -0300

    test2

---
+++ app/[id]/page.tsx
@@ -0,0 +1,28 @@
+ 
+ type Props = {
+  params: Promise<{ id: string }>
+}
+
+export async function generateStaticParams() {
+  return [
+    { id: '10000045' }
+  ]
+}
+
+export default async function Page({
+  params,
+}: Props) {
+   const id = (await params).id;
+  const res = await fetch(
+    `https://gi.yatta.moe/api/v2/pt/avatar/${id}?vh=63F3`,
+    { cache: 'force-cache' }
+  )
+
+  const character = await res.json()
+console.log(character.data.fetter.title)
+  return (
+    <ul>
+      <li>{}</li>
+    </ul>
+  )
+}
